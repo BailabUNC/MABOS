f1dd45d81f7b32a2962e824f2811e2952be8691b
ArjunPutcha <arjunputcha@gmail.com>
2023-05-30 11:56:40 -0700

    initial attempt at moving average filter with burst sampling

--- firmware/arduino/main/main.ino
+++ firmware/arduino/main/main.ino
@@ -1,17 +1,25 @@
 #include <Arduino.h>
 #include <Adafruit_TinyUSB.h> // for Serial
 
+// Pin values for led/pd
 #define red_led 7
 #define red_adc A0
-int adcvalue = 0;
+// constants for adjusting ADC input
 float MV_PER_LSB (0.73242188F); // 3.0V ADC range and 12-bit ADC resolution = 3000mV/4096
 #define VBAT_DIVIDER (0.5F)
 #define VBAT_DIVIDER_COMP (2.0F)
 #define REAL_VBAT_MV_PER_LSB (VBAT_DIVIDER_COMP * MV_PER_LSB)
 float raw;
-
+// constants for moving average filter & sample rate
 #define sampleSize 5
-#define sampleRate 10
+#define sampleRate 5
+#define window_size 10
+int idx = 0;
+int burst_sum = 0;
+int burst_avg = 0;
+int red_sum = 0;
+int red_adc_arr[sampleSize];
+int red_avg;
 
 float readADC(void) {
   raw = analogRead(red_adc);
@@ -27,26 +35,27 @@ void setup() {
   delay(1);
   readADC();
 }
+
 void loop() {
-  float red_adc_arr[sampleSize];
-  float average;
-  float median=0;
-  float sum;
+
     digitalWrite(red_led, HIGH);
 
     for (int i=0; i<sampleSize; i++){
-      red_adc_arr[i] = readADC();
-      delay(sampleRate/10);
+      burst_sum += readADC();
+      delay(100);
     }
+    burst_avg = (burst_sum/sampleSize);
+    burst_sum = 0;
+    red_sum = red_sum - red_adc_arr[idx];
+    red_adc_arr[idx] = burst_avg;
+    red_sum += burst_avg;
+    idx = (idx + 1) % window_size;
 
-    for (int i; i<sampleSize; i++){
-      sum = sum + red_adc_arr[i];
-    }
-    average = (sum/sampleSize);
-    Serial.println(average);
+    red_avg = red_sum/window_size;
+    Serial.println(red_avg);
     Serial.flush();
     digitalWrite(red_led, LOW);
-    delay(5);
+    delay(500);
 }
 
 
